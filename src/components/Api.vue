a8e8b537a179a8bc33cf1351b855154d1967c9c1
minabe <minabe@bringup.jp>
2019-02-06 16:01:26 +0900

    Change Api

--- src/components/Api.vue
+++ src/components/Api.vue
@@ -1,40 +1,48 @@
 <template>
   <div id="api-page">
-    Api Page
-    <div
-      v-for="currency in info"
-      :key="currency.code"
-      class="currency"
-    >
-      {{ currency.description }}:
-      <span class="lighten">
-        <span v-html="currency.symbol"></span>{{ currency.rate_float}}
-      </span>
-    </div>
+    <h1>Api Page</h1>
+    <label>郵便番号</label>
+    <input type="text" name="zipcode" value="" v-model="zipcode" v-on:change="getAddress">
+    <p>{{address}}</p>
   </div>
 </template>
 
 <script>
+import HttpMixin from "../mixins/HttpMixin"
+
 export default {
   name: 'Api',
+  mixins: [HttpMixin],
   data() {
     return {
-      info: [],
+      api_url: 'https://api.zipaddress.net/',
+      zipcode: '',
+      address: '',
       errro: [],
       errored: false
     }
   },
-  mounted () {
-    this.axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => {
-        this.info = response.data.bpi
-      })
-      .catch(error => {
-        this.error = error
-        this.errored = true
-      })
-      .finally(() => this.loading = false)
+  methods: {
+    getAddress() {
+      this.axios
+        .get(this.api_url + '?zipcode=' + this.zipcode)
+        .then(response => {
+          this.address = response.data.data.fullAddress
+        })
+        .catch(error => {
+          this.error = error
+          this.errored = true
+          this.address = '見つかりませんでした'
+        })
+        .finally(() => this.loading = false)
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  label {
+    display: block;
+    padding-bottom: 10px;
+  }
+</style>
---
+++ src/mixins/HttpMixin.js
@@ -0,0 +1,6 @@
+export default {
+  mounted() {
+      // set axios header API側でのajax判別の為、付与
+      // this.axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest';
+  }
+}
